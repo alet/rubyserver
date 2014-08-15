0d061cfa31d84c6693875f2aa1946330b2ecbd48
Alexander Galato <alexander.galato@gmail.com>
2014-08-15 18:30:27 +0300

    Fix copyright

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,6 @@
 # Cookbook Name:: rubyserver
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 
 package "libcurl4-openssl-dev"
 include_recipe "rvm::system"
